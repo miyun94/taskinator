6ed3a654dcdf87b8f68e06ecb5d434ef1db127b4
echo <miyunniecho@gmail.com>
2021-01-05 21:37:18 -0800

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,49 @@
-let formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var taskFormHandler = function(event) {
 
-var createTaskHandler = function(event) { 
-    //adding the .preventDefault stops the browser from carrying out its default behavior, whic is to refresh every time
-    event.preventDefault();
+    //adding the .preventdefault stops the browswer from carrying out tis default behavior which is to refresh every time
+  event.preventDefault();
+        //using [] in selector, we are selecting html element by one of its attributes
+        //i.e. in this case the one with the task-name attribute
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    //using [] in selector, we are selecting html element by one of its attributes
-    //i.e. in this case the one with the task-name attribute
-    let taskNameInput = document.querySelector("input[name='task-name']").Value;
-    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+    }
 
-    //create item list
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; 
+    formEl.reset();
 
-    //create div to hold task infor and add to list item 
-    let taskInfoEl = document.createElement("div"); 
-    //give it a class name 
-    taskInfoEl.className = "task-info"; 
-    //add HTML content to div
-    taskInfoEl.innerHTML="<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput "</span>";
-    listItemEl.appendChild(taskInfoEl);
+  // package up data as an object
+  var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+  };
 
-    //add entire list item to list
-    tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
 
+var createTaskEl = function (taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
 //we changed from "click" to "submt" since we made the whole form the event listeners, 
 //if we did "click" then any time any where the form is clicked on, it would cause it to go off
 //so we specified its only when submit it hit 
-formEl.addEventListener("submit" createTaskHandler);
