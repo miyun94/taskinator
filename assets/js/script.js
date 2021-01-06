7d35d8de6d4395f89d35bb3413a51000e4dfecf5
echo <miyunniecho@gmail.com>
2021-01-05 21:18:49 -0800

    task input js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
-let formEl = document.querySelector("task-form");
+let formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
 
 var createTaskHandler = function(event) { 
     //adding the .preventDefault stops the browser from carrying out its default behavior, whic is to refresh every time
     event.preventDefault();
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
 
+    //using [] in selector, we are selecting html element by one of its attributes
+    //i.e. in this case the one with the task-name attribute
+    let taskNameInput = document.querySelector("input[name='task-name']").Value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create item list
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item"; 
+
+    //create div to hold task infor and add to list item 
+    let taskInfoEl = document.createElement("div"); 
+    //give it a class name 
+    taskInfoEl.className = "task-info"; 
+    //add HTML content to div
+    taskInfoEl.innerHTML="<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl); 
   }; 
 
 //we changed from "click" to "submt" since we made the whole form the event listeners, 
